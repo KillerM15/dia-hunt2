d62b2c2c46447741080b66e6bc93f095ffe9d7e0
unknown <killerm15@protonmail.ch>
2020-07-09 22:40:19 +0200

    Added DiaHuntExecutor to test commands

--- src/main/java/killerm/minecraft/DiaHuntPlugin.java
+++ src/main/java/killerm/minecraft/DiaHuntPlugin.java
@@ -1,10 +1,12 @@
 package killerm.minecraft;
 
+import killerm.minecraft.commands.DiaHuntExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiaHuntPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
+        this.getCommand("diahunt").setExecutor(new DiaHuntExecutor());
     }
 
     @Override
---
+++ src/main/java/killerm/minecraft/commands/DiaHuntExecutor.java
@@ -0,0 +1,13 @@
+package killerm.minecraft.commands;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DiaHuntExecutor implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String label, String[] args) {
+        
+        return true;
+    }
+}
