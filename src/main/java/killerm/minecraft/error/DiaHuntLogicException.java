6852e0a4600bafd70aa4498e4b8e2978e4e4b4d1
unknown <killerm15@protonmail.ch>
2020-07-09 14:21:34 +0200

    Added DiaHuntLogicException which is thrown if user does something he shouldn't do

---
+++ src/main/java/killerm/minecraft/error/DiaHuntLogicException.java
@@ -0,0 +1,7 @@
+package killerm.minecraft.error;
+
+public class DiaHuntLogicException extends RuntimeException {
+    public DiaHuntLogicException(String message) {
+        super(message);
+    }
+}
