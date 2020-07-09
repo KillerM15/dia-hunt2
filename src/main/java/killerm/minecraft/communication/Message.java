38b65641abcbfc1182946c1d4e82dfe7cd8ed087
unknown <killerm15@protonmail.ch>
2020-07-09 21:54:06 +0200

    Added colors and chat prefixes to messages

--- src/main/java/killerm/minecraft/communication/Message.java
+++ src/main/java/killerm/minecraft/communication/Message.java
@@ -5,9 +5,27 @@ import killerm.minecraft.helper.CollectionDelimiter;
 
 // Intentional code duplication and ugly inner class to increase readability
 public class Message {
+    public static final String GOLD = "§6";
+    public static final String GREY = "§7";
+    public static final String DARK_RED = "§4";
+    public static final String RED = "§c";
+    public static final String DARK_BLUE = "§1";
+    public static final String AQUA = "§b";
+    public static final String BLUE = "§9";
+    public static final String DARK_AQUA = "§3";
+    public static final String FAT = "§l";
+    public static final String RESET = "§r";
+    public static final String TEAM_AQUA = BLUE;
+    public static final String TEAM_LAVA = RED;
+
     public static final String COLLECTION_DELIMITER = ", ";
     public static final String VALID_COMMANDTYPES = "Valid commands: " + CollectionDelimiter.delimit(CommandType.values(), CommandType::getCommandInChat);
     public static final String COMMANDTYPE_NOT_VALID = "This command isn't valid! " + VALID_COMMANDTYPES;
     public static final String HELP = "help";
     public static final String HELP_HELP = "";
+
+    public static final String BROADCAST = AQUA + FAT + "◆Dia Hunt◆ " + DARK_AQUA;
+    public static final String BROADCAST_ERROR = BROADCAST + DARK_RED + "ERROR: " + DARK_AQUA;
+    public static final String PLAYER = AQUA + FAT + "◆" + GREY;
+    public static final String PLAYER_ERROR = PLAYER + RESET + DARK_RED + " ERROR: " + GREY;
 }
