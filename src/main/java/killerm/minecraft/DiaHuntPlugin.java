487901d670a4eac8dea319cdda7279f85ffb16e2
unknown <killerm15@protonmail.ch>
2020-07-09 14:13:11 +0200

    Added DiaHuntPlugin which enables and disables the plugin

---
+++ src/main/java/killerm/minecraft/DiaHuntPlugin.java
@@ -0,0 +1,13 @@
+package killerm.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DiaHuntPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
