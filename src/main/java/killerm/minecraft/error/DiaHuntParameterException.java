7897d9763c409b0741fb05c8ff00b4a400c0e338
unknown <killerm15@protonmail.ch>
2020-07-09 14:22:13 +0200

    Added DiaHuntParameterException which is thrown if user puts in an invalid command

---
+++ src/main/java/killerm/minecraft/error/DiaHuntParameterException.java
@@ -0,0 +1,7 @@
+package killerm.minecraft.error;
+
+public class DiaHuntParameterException extends RuntimeException {
+    public DiaHuntParameterException(String message) {
+        super(message);
+    }
+}
