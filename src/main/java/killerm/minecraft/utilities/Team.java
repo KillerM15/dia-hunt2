fd6fc56f8c6418d962af1343504cfb0a130ee035
unknown <killerm15@protonmail.ch>
2020-07-11 16:54:23 +0200

    Added Team (aqua and java) * Added tests for Team

---
+++ src/main/java/killerm/minecraft/utilities/Team.java
@@ -0,0 +1,23 @@
+package killerm.minecraft.utilities;
+
+import killerm.minecraft.communication.Message;
+import killerm.minecraft.error.DiaHuntParameterException;
+
+public enum Team {
+    AQUA, LAVA;
+
+    @Override
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
+
+    public static Team getTeam(String teamString) {
+        for (Team t : Team.values()) {
+            if (teamString.equals(t.toString())) {
+                return t;
+            }
+        }
+
+        throw new DiaHuntParameterException(Message.TEAM_NOT_VALID);
+    }
+}
---
+++ src/test/java/killerm/minecraft/utilities/TeamTest.java
@@ -0,0 +1,36 @@
+package killerm.minecraft.utilities;
+
+import killerm.minecraft.communication.Message;
+import killerm.minecraft.error.DiaHuntParameterException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamTest {
+    @Test
+    public void GIVEN_Teams_WHEN_toString_THEN_toLower() {
+        assertEquals("lava", Team.LAVA.toString());
+        assertEquals("aqua", Team.AQUA.toString());
+    }
+
+    @Test
+    public void GIVEN_valid_teamString_WHEN_getTeam_THEN_return_correct_team() {
+        assertEquals(Team.LAVA, Team.getTeam("lava"));
+    }
+
+    @Test
+    public void GIVEN_invalid_teamString_WHEN_getTeam_THEN_Exception() {
+        // GIVEN
+        String teamString = "invalid";
+
+        // WHEN
+        DiaHuntParameterException thrown = assertThrows(
+                DiaHuntParameterException.class,
+                () -> Team.getTeam(teamString),
+                "Expected getTeam to throw, but it didn't"
+        );
+
+        // THEN
+        assertTrue(thrown.getMessage().equals(Message.TEAM_NOT_VALID));
+    }
+}
