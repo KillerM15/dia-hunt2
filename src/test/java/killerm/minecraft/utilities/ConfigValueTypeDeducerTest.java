b01185464d8f2013b45975e4bf37e3074b99ab70
unknown <killerm15@protonmail.ch>
2020-07-12 13:56:36 +0200

    Changed ConfigDeducer to ConfigValueTypeDeducer

--- src/test/java/killerm/minecraft/utilities/ConfigValueTypeDeducerTest.java
+++ src/test/java/killerm/minecraft/utilities/ConfigValueTypeDeducerTest.java
@@ -2,26 +2,26 @@ package killerm.minecraft.utilities;
 
 import org.junit.jupiter.api.Test;
 
-class ConfigValueTypeTest {
-    ConfigDeducer configDeducer = new ConfigDeducer("location");
+class ConfigValueTypeDeducerTest {
+    ConfigValueTypeDeducer configValueTypeDeducer = new ConfigValueTypeDeducer("location");
 
     @Test
     public void GIVEN_location_WHEN_isLocation_THEN_true() {
-        assert(configDeducer.isLocation("location"));
+        assert(configValueTypeDeducer.isLocation("location"));
     }
 
     @Test
     public void GIVEN_double_WHEN_isNumber_THEN_true() {
-        assert(configDeducer.isNumber("2229.9930"));
+        assert(configValueTypeDeducer.isNumber("2229.9930"));
     }
 
     @Test
     public void GIVEN_random_string_WHEN_isNumber_THEN_false() {
-        assert(!configDeducer.isNumber("2229.9930e"));
+        assert(!configValueTypeDeducer.isNumber("2229.9930e"));
     }
 
     @Test
     public void GIVEN_Integer_WHEN_isNumber_THEN_true() {
-        assert(configDeducer.isNumber("2229"));
+        assert(configValueTypeDeducer.isNumber("2229"));
     }
 }
