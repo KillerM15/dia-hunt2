e60b9e2879124ba8860a16d8b31c8ade66c8e824
unknown <killerm15@protonmail.ch>
2020-07-12 13:58:55 +0200

    Refactored Message * Added messages

--- src/main/java/killerm/minecraft/communication/Message.java
+++ src/main/java/killerm/minecraft/communication/Message.java
@@ -25,21 +25,25 @@ public class Message {
 
     public static final String PREFIX_BROADCAST = AQUA + FAT + "◆Dia Hunt◆ " + DARK_AQUA;
     public static final String PREFIX_BROADCAST_ERROR = PREFIX_BROADCAST + DARK_RED + "ERROR: " + DARK_AQUA;
-    public static final String PREFIX_TELL = AQUA + FAT + "◆" + GREY;
-    public static final String PREFIX_TELL_ERROR = PREFIX_TELL + RESET + DARK_RED + " ERROR: " + GREY;
+    public static final String PREFIX_TELL = AQUA + FAT + "◆ " + GREY;
+    public static final String PREFIX_TELL_ERROR = PREFIX_TELL + RESET + DARK_RED + "ERROR: " + GREY;
 
     public static final String COLLECTION_DELIMITER = ", ";
     public static final String VALID_COMMANDTYPES = "Valid commands: " + CollectionDelimiter.delimit(CommandType.values(), CommandType::toString);
     public static final String COMMANDTYPE_NOT_VALID = "This command isn't valid! " + VALID_COMMANDTYPES;
     public static final String COMMAND_NOT_IMPLEMENTED = "This command hasn't been implemented yet!";
     public static final String VALID_DIACONFIGS = "Valid configurations: " + CollectionDelimiter.delimit(DiaConfig.values(), DiaConfig::toString);
-    public static final String DIACONFIG_NOT_VALID = "This command isn't valid! " + VALID_DIACONFIGS;
+    public static final String DIACONFIG_NOT_VALID = "This configuration isn't valid! " + VALID_DIACONFIGS;
     public static final String DIACONFIG_NOT_IMPLEMENTED = "This command hasn't been implemented yet!";
+    public static final String DESCRIPTION_EMPTY = "";
+    public static final String DESCRIPTION_CONFIG = "Change config values. Usage: /diahunt config KEY (VALUE)";
+
     public static final String VALID_TEAMS = "Valid teams: " + CollectionDelimiter.delimit(Team.values(), Team::toString);
     public static final String TEAM_NOT_VALID = "This team isn't valid! " + VALID_TEAMS;
     public static final String IS_SET_TO = " is set to ";
     public static final String CONFIG_LOCATION_INDICATOR = "location";
-
-    public static final String DESCRIPTION_EMPTY = "";
-    public static final String DESCRIPTION_CONFIG = "Change config values. Usage: /diahunt config KEY (VALUE)";
+    public static final String TOO_MANY_ARGS = "You entered too many arguments!";
+    public static final String AND = " and ";
+    public static final String NOT_SAME_TYPE = " are not the same type!";
+    //public static final String WRONG_TYPE = "You didn't enter a value!";
 }
