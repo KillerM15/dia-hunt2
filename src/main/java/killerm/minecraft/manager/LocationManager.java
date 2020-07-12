4ac9bf313d6415a7bd0de8c9ba387891549d8cc9
unknown <killerm15@protonmail.ch>
2020-07-12 15:21:13 +0200

    Added teleport to DiaConfig enum to LocationManager

--- src/main/java/killerm/minecraft/manager/LocationManager.java
+++ src/main/java/killerm/minecraft/manager/LocationManager.java
@@ -1,5 +1,6 @@
 package killerm.minecraft.manager;
 
+import killerm.minecraft.data.DiaConfig;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -12,7 +13,17 @@ public class LocationManager {
         }
     }
 
-    private void teleport(Player player, Location loc) {
+    public void teleport(Player player, Location loc) {
         player.teleport(loc);
+    }
+
+    public void teleport(Collection<Player> players, DiaConfig diaConfig) {
+        for (Player player : players) {
+            teleport(player, diaConfig);
+        }
+    }
+
+    public void teleport(Player player, DiaConfig diaConfig) {
+        player.teleport((Location) diaConfig.get());
     }
 }
