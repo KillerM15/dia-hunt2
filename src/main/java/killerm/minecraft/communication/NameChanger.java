a35e1a7cdbed05c270450ad2fbc3d7e615f17fdf
unknown <killerm15@protonmail.ch>
2020-07-12 16:01:15 +0200

    Replaced String + with StringBuilder in NameChanger

--- src/main/java/killerm/minecraft/communication/NameChanger.java
+++ src/main/java/killerm/minecraft/communication/NameChanger.java
@@ -5,25 +5,24 @@ import org.bukkit.entity.Player;
 
 public class NameChanger {
     public void setPlayerColor(Player player, Team team) {
+        StringBuilder sb = new StringBuilder();
+
         if (team == Team.AQUA) {
-            String newName = Message.TEAM_AQUA
-                    + Message.SYMBOL_WATER
-                    + player.getName()
-                    + Message.SYMBOL_WATER
-                    + Message.RESET;
-
-            player.setDisplayName(newName);
-            player.setPlayerListName(newName);
+            sb.append(Message.TEAM_AQUA);
+            sb.append(Message.SYMBOL_WATER);
+            sb.append(player.getName());
+            sb.append(Message.SYMBOL_WATER);
+            sb.append(Message.RESET);
         } else if (team == Team.LAVA) {
-            String newName = Message.TEAM_LAVA
-                    + Message.SYMBOL_FIRE
-                    + player.getName()
-                    + Message.SYMBOL_FIRE
-                    + Message.RESET;
-
-            player.setDisplayName(newName);
-            player.setPlayerListName(newName);
+            sb.append(Message.TEAM_LAVA);
+            sb.append(Message.SYMBOL_FIRE);
+            sb.append(player.getName());
+            sb.append(Message.SYMBOL_FIRE);
+            sb.append(Message.RESET);
         }
+
+        player.setDisplayName(sb.toString());
+        player.setPlayerListName(sb.toString());
     }
 
     public void reset(Player player) {
