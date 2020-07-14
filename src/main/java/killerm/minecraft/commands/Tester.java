1d6bb7441ac68a43ad7a2fcd1131d20070fefa1e
BuildTools <killerm15@protonmail.ch>
2020-07-14 14:06:49 +0200

    Changed public to private in Tester

--- src/main/java/killerm/minecraft/commands/Tester.java
+++ src/main/java/killerm/minecraft/commands/Tester.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 
 // Is used for testing commands while developing
 public class Tester {
-    Printer printer = new Printer();
+    private Printer printer = new Printer();
 
     public void test1(Player player) {
     }
