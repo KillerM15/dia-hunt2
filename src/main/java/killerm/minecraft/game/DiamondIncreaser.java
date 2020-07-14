70ee125d3ed5dd48bc85fc4b2e37b1a3e8238c0d
BuildTools <killerm15@protonmail.ch>
2020-07-14 14:06:19 +0200

    Refactored DiamondIncreaser so it takes ticksPerDiamond by itself from config

--- src/main/java/killerm/minecraft/game/DiamondIncreaser.java
+++ src/main/java/killerm/minecraft/game/DiamondIncreaser.java
@@ -1,25 +1,28 @@
 package killerm.minecraft.game;
 
 import killerm.minecraft.DiaHuntPlugin;
+import killerm.minecraft.data.DiaConfig;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class DiamondIncreaser {
-    private DiaChestGameData diaChestGameData;
     private PlayerGameData playerGameData;
+    private DiaChestGameData diaChestGameData;
     private DiamondIndicator diamondIndicator;
     private ItemGiver itemGiver;
     private BukkitRunnable increaserThread;
 
-    public DiamondIncreaser(DiaChestGameData diaChestGameData, PlayerGameData playerGameData) {
-        this.diaChestGameData = diaChestGameData;
+    public DiamondIncreaser(PlayerGameData playerGameData, DiaChestGameData diaChestGameData) {
         this.playerGameData = playerGameData;
+        this.diaChestGameData = diaChestGameData;
         this.diamondIndicator = new DiamondIndicator();
         this.itemGiver = new ItemGiver();
     }
 
-    public void start(int ticksPerDiamond) {
+    public void start() {
+        int ticksPerDiamond = (int) (double) DiaConfig.TICKS_PER_DIAMOND.get();
+
         this.increaserThread = new BukkitRunnable() {
             public void run() {
                 addDiamondToPlayers();
