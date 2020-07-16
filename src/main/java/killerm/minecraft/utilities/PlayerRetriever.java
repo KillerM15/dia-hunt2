e7903304577031886efe2b42866d606d4a1ef947
BuildTools <killerm15@protonmail.ch>
2020-07-16 22:58:08 +0200

    Added cast for PlayerRetriever

--- src/main/java/killerm/minecraft/utilities/PlayerRetriever.java
+++ src/main/java/killerm/minecraft/utilities/PlayerRetriever.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import java.util.Collection;
 
 public class PlayerRetriever {
-    public Collection<? extends Player> retrieveOnlinePlayers() {
-        return Bukkit.getOnlinePlayers();
+    public Collection<Player> retrieveOnlinePlayers() {
+        return (Collection) Bukkit.getOnlinePlayers();
     }
 }
