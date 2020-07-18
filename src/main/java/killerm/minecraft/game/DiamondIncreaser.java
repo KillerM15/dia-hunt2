09b4d3019d9a56f0f7e888c3fd70af92d99b35b7
BuildTools <killerm15@protonmail.ch>
2020-07-18 15:44:04 +0200

    Changed Diamond spawn rate in DiamondIncreaser * Added tests for DiamondIncreaser

--- src/main/java/killerm/minecraft/game/DiamondIncreaser.java
+++ src/main/java/killerm/minecraft/game/DiamondIncreaser.java
@@ -2,6 +2,7 @@ package killerm.minecraft.game;
 
 import killerm.minecraft.DiaHuntPlugin;
 import killerm.minecraft.data.DiaConfig;
+import killerm.minecraft.manager.ScoreboardManager;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -12,12 +13,22 @@ public class DiamondIncreaser {
     private DiamondIndicator diamondIndicator;
     private ItemGiver itemGiver;
     private BukkitRunnable increaserThread;
+    private ScoreboardManager scoreboardManager;
 
     public DiamondIncreaser(PlayerGameData playerGameData, DiaChestGameData diaChestGameData) {
         this.playerGameData = playerGameData;
         this.diaChestGameData = diaChestGameData;
         this.diamondIndicator = new DiamondIndicator();
         this.itemGiver = new ItemGiver();
+        this.scoreboardManager = new ScoreboardManager();
+    }
+
+    public DiamondIncreaser(PlayerGameData playerGameData, DiaChestGameData diaChestGameData, DiamondIndicator diamondIndicator, ItemGiver itemGiver, ScoreboardManager scoreboardManager) {
+        this.playerGameData = playerGameData;
+        this.diaChestGameData = diaChestGameData;
+        this.diamondIndicator = diamondIndicator;
+        this.itemGiver = itemGiver;
+        this.scoreboardManager = scoreboardManager;
     }
 
     public void start() {
@@ -25,25 +36,41 @@ public class DiamondIncreaser {
 
         this.increaserThread = new BukkitRunnable() {
             public void run() {
-                addDiamondToPlayers();
-                addDiamondToChests();
+                addDiamonds();
             }
         };
 
         increaserThread.runTaskTimer(DiaHuntPlugin.getInstance(), 0, ticksPerDiamond);
     }
 
-    private void addDiamondToPlayers() {
+    public void startMocked() {
+        addDiamonds();
+    }
+
+    private void addDiamonds() {
+        addDiamondsToPlayers();
+        addDiamondsToChests();
+    }
+
+    private void addDiamondsToPlayers() {
         for (Player player : playerGameData.players()) {
-            if (diamondIndicator.hasDiamonds(player)) {
-                itemGiver.giveDia(player);
+            if (!diamondIndicator.hasDiamonds(player)) {
+                continue;
             }
+
+            itemGiver.giveDia(player);
+            scoreboardManager.refresh(player);
         }
     }
 
-    private void addDiamondToChests() {
+    private void addDiamondsToChests() {
         for (ShulkerBox shulkerBox : diaChestGameData.getShulkerBoxes()) {
-            if (diamondIndicator.hasDiamonds(shulkerBox)) {
+            if (!diamondIndicator.hasDiamonds(shulkerBox)) {
+                continue;
+            }
+
+            // Chest gets amountOfPlayers diamonds
+            for (int i = 0; i < playerGameData.amountOfPlayers(); i++) {
                 itemGiver.giveDia(shulkerBox);
             }
         }
