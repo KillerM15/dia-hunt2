2eacff3e066a8aea851b5906ccbe3465b1dd5e05
BuildTools <killerm15@protonmail.ch>
2020-07-19 19:27:40 +0200

    Added slotsInInventoryRow in MinecraftConstants

--- src/main/java/killerm/minecraft/utilities/MinecraftConstants.java
+++ src/main/java/killerm/minecraft/utilities/MinecraftConstants.java
@@ -1,5 +1,6 @@
 package killerm.minecraft.utilities;
 
 public class MinecraftConstants {
-    public static int ticksPerSecond = 20;
+    public static final int ticksPerSecond = 20;
+    public static final int slotsInInventoryRow = 9;
 }
