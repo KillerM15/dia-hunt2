23190a547017bbd547587bdc8a25051d52ced754
BuildTools <killerm15@protonmail.ch>
2020-07-20 22:41:08 +0200

    Added NotEnoughItemsException * Added ItemNotFoundException

---
+++ src/main/java/killerm/minecraft/error/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package killerm.minecraft.error;
+
+public class ItemNotFoundException extends RuntimeException {
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+}
