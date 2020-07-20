4552a3184f4625ad5285463ef9c3684e14b5e07b
BuildTools <killerm15@protonmail.ch>
2020-07-20 22:52:09 +0200

    Added shop item to GameItem * Changed messages of GameItem

--- src/main/java/killerm/minecraft/game/item/GameItem.java
+++ src/main/java/killerm/minecraft/game/item/GameItem.java
@@ -3,6 +3,7 @@ package killerm.minecraft.game.item;
 import killerm.minecraft.communication.Message;
 import killerm.minecraft.game.data.Team;
 import killerm.minecraft.game.shop.ItemCategory;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -78,56 +79,77 @@ public class GameItem { // TODO: Nice descriptions and effects with enchantments
             glassPane = new ItemStack(Material.RED_STAINED_GLASS_PANE);
         }
 
-        setDisplayName(glassPane, " ");
-        setLore(glassPane);
+        setDisplayName(glassPane, Message.RESET + Message.ITALIC + Message.GREY + Message.SHOP_INSTRUCTION_LEFT_CLICK);
+        setLore(glassPane, Message.RESET + Message.GREY + Message.SHOP_INSTRUCTION_RIGHT_CLICK);
         return glassPane;
     }
 
     public ItemStack shopBuildingItem() {
         ItemStack itemStack = new ItemStack(Material.BRICKS);
-        setDisplayName(itemStack, Message.RESET + Message.GOLD + Message.BOLD + ItemCategory.BUILDING.toString());
+        setDisplayName(itemStack, Message.RESET + Message.ITALIC + Message.YELLOW + Message.BOLD + ItemCategory.BUILDING.toString());
         setLore(itemStack, Message.GREY + ItemCategory.BUILDING.getDescription());
         return itemStack;
     }
 
     public ItemStack shopMeleeItem() {
         ItemStack itemStack = new ItemStack(Material.STICK);
-        setDisplayName(itemStack, Message.RESET + Message.GOLD + Message.BOLD + ItemCategory.MELEE.toString());
+        setDisplayName(itemStack, Message.RESET + Message.YELLOW + Message.BOLD + ItemCategory.MELEE.toString());
         setLore(itemStack, Message.GREY + ItemCategory.MELEE.getDescription());
         return itemStack;
     }
 
     public ItemStack shopRangedItem() {
         ItemStack itemStack = new ItemStack(Material.BOW);
-        setDisplayName(itemStack, Message.RESET + Message.GOLD + Message.BOLD + ItemCategory.RANGED.toString());
+        setDisplayName(itemStack, Message.RESET + Message.YELLOW + Message.BOLD + ItemCategory.RANGED.toString());
         setLore(itemStack, Message.GREY + ItemCategory.RANGED.getDescription());
         return itemStack;
     }
 
     public ItemStack shopEffectsItem() {
         ItemStack itemStack = new ItemStack(Material.PLAYER_HEAD);
-        setDisplayName(itemStack, Message.RESET + Message.GOLD + Message.BOLD + ItemCategory.EFFECTS.toString());
+        setDisplayName(itemStack, Message.RESET + Message.YELLOW + Message.BOLD + ItemCategory.EFFECTS.toString());
         setLore(itemStack, Message.GREY + ItemCategory.EFFECTS.getDescription());
         return itemStack;
     }
 
     public ItemStack shopProtectionItem() {
         ItemStack itemStack = new ItemStack(Material.SHIELD);
-        setDisplayName(itemStack, Message.RESET + Message.GOLD + Message.BOLD + ItemCategory.PROTECTION.toString());
+        setDisplayName(itemStack, Message.RESET + Message.YELLOW + Message.BOLD + ItemCategory.PROTECTION.toString());
         setLore(itemStack, Message.GREY + ItemCategory.PROTECTION.getDescription());
         return itemStack;
     }
 
     public ItemStack shopTrickyItem() {
         ItemStack itemstack = new ItemStack(Material.REDSTONE_TORCH);
-        setDisplayName(itemstack, Message.RESET + Message.GOLD + Message.BOLD + ItemCategory.TRICKY.toString());
+        setDisplayName(itemstack, Message.RESET + Message.YELLOW + Message.BOLD + ItemCategory.TRICKY.toString());
         setLore(itemstack, Message.GREY + ItemCategory.TRICKY.getDescription());
         return itemstack;
     }
 
     public ItemStack melon() { // TODO: remove as soon as real items are implemented
         ItemStack itemstack = new ItemStack(Material.MELON);
-        setDisplayName(itemstack, Message.GREEN + "MELONE!");
+        setDisplayName(itemstack, Message.DARK_GREEN + "MELONE!");
+        setLore(itemstack, Message.GREEN + "A melon", Message.ITALIC + Message.AQUA + "20 Diamonds");
+        itemstack.setAmount(64);
         return itemstack;
+    }
+
+    public ItemStack shop(Team team) {
+        ItemStack itemstack = new ItemStack(Material.ENDER_CHEST);
+        setDisplayName(itemstack, getShopName(team));
+        return itemstack;
+    }
+
+    private String getShopName(Team team) {
+        StringBuilder sb = new StringBuilder();
+
+        String color = team.equals(Team.LAVA) ? Message.TEAM_LAVA : Message.TEAM_AQUA;
+        sb.append(color);
+        sb.append(Message.BOLD);
+        sb.append(StringUtils.capitalize(team.toString()));
+        sb.append(Message.SPACE);
+        sb.append(Message.SHOP_NAME);
+
+        return sb.toString();
     }
 }
